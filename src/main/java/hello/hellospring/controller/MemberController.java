b9e6aa4f06056d97cdaa49577710a0648b7b652b
miderK <freedicenice@naver.com>
2021-01-01 21:11:55 +0900

    컴포넌트 스캔과 자동 의존관계 설정 MemberController 생성 MemberService @Service 어노테이션 추가 MemoryMemberRepository @Repository 어노테이션추가

---
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -0,0 +1,17 @@
+package hello.hellospring.controller;
+
+import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+// 스프링이 컨트롤러 @이 있으면 관리함 스프링 컨테이너에서 스프링 빈이 관리
+@Controller
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+}
--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -3,15 +3,19 @@ package hello.hellospring.service;
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService {
     private final MemberRepository memberRepository;
 
     // 멤버리파티토리를 외부에서 가져와서 넣어주는걸로
     // 이런것을 DI (Dependency Injection)이라 함
+    @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
