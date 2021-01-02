11e7bfa8892c8dd3df7274244486102cd138499c
MIDER\freed <freedicenice@naver.com>
2021-01-02 23:23:39 +0900

    H2 DB설치 gradle에 H2 DB설정 application.properties에 H2 DB 설정 추가 테이블 내용 ddl.sql에 추가 JdbcMemberRepository.java에 고대의 개발방식으로 소스코드 추가 SpringConfig.java에서 DB설정변경

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,13 +1,24 @@
 package hello.hellospring;
 
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,7 +27,11 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        // 메모리에 저장하는 테스트용
+        //return new MemoryMemberRepository();
+
+        // 순수JDBC로 설정
+        return new JdbcMemberRepository(dataSource);
     }
 
 }
